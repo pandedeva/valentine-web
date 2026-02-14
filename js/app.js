7d64ba30187c117716e4e6495970f53e3b262b0d
pandedeva <devapande66@gmail.com>
2026-02-14 11:31:11 +0700

    update valentine web

--- js/app.js
+++ js/app.js
@@ -4,75 +4,109 @@ document.addEventListener("DOMContentLoaded", () => {
   const gif = document.querySelector("#gif");
   const text = document.querySelector("#text");
   const vid = document.querySelector("video");
+  const textBox = document.querySelector(".text-one");
   let count = 2;
 
   const gifs = [
     "resources/cat-heart.gif",
     "resources/rusure.gif",
-    "resources/3shocked-1.gif",
-    "resources/4.crying.gif",
-    "resources/5.crying.gif",
+    "resources/onana.gif",
+    "resources/Sad.gif",
+    "resources/speed.gif",
     "resources/idc.gif",
   ];
 
-  // to load the gifs faster
-  gifs.forEach((gifSrc) => {
-    const img = new Image();
-    img.src = gifSrc;
+  // 🔥 hanya fade-in ketika source berubah
+  gif.style.transition = "opacity 0.3s ease-in-out";
+
+  // preload GIFs
+  const preloadGifs = gifs.map((src) => {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = () => resolve();
+    });
   });
 
-  // changes for when NO button is clicked
-  no.addEventListener("click", () => {
-    if (count == 2) {
-      gif.src = "resources/rusure.gif"; // gif credit: https://knowyourmeme.com/photos/2738959-mr-fresh-side-eye-cat
-      text.innerHTML = "Kok no????🤨";
-      yes.style.height = "65%";
-      yes.style.width = "60%";
-      no.style.width = "30%";
+  Promise.all(preloadGifs).then(() => {
+    // semua GIF siap, aktifkan event listener
+    no.addEventListener("click", () => {
+      let src, message;
+      let yesHeight, yesWidth, noWidth, noFont;
+
+      switch (count) {
+        case 2:
+          src = "resources/rusure.gif";
+          message = "Kok no????🤨";
+          yesHeight = "65%";
+          yesWidth = "60%";
+          noWidth = "30%";
+          break;
+        case 3:
+          src = "resources/onana.gif";
+          message = "Sengajaaa yhhh?🤨🤨🤨";
+          yesHeight = "70%";
+          yesWidth = "70%";
+          noWidth = "20%";
+          break;
+        case 4:
+          src = "resources/Sad.gif";
+          message = "Sedihhh 😭😭";
+          yesHeight = "80%";
+          yesWidth = "80%";
+          noWidth = "10%";
+          noFont = "4vh";
+          break;
+        case 5:
+          src = "resources/speed.gif";
+          message = ":)";
+          yesHeight = "90%";
+          yesWidth = "96%";
+          no.style.display = "none";
+          break;
+      }
+
+      // fade-in hanya ketika source baru
+      gif.style.opacity = 0;
+      gif.src = src;
+      gif.onload = () => {
+        gif.style.opacity = 1;
+      };
+
+      text.innerHTML = message;
+
+      if (yesHeight) yes.style.height = yesHeight;
+      if (yesWidth) yes.style.width = yesWidth;
+      if (noWidth) no.style.width = noWidth;
+      if (noFont) no.style.fontSize = noFont;
+
       count++;
-    } else if (count == 3) {
-      gif.src = "resources/onana.gif"; // gif credit: https://tenor.com/view/tkthao219-peach-goma-gif-25008901
-      text.innerHTML = "Sengajaaa yhhh?🤨🤨🤨";
-      yes.style.height = "70%";
-      yes.style.width = "70%";
-      no.style.width = "20%";
-      count++;
-    } else if (count == 4) {
-      gif.src = "resources/Sad.gif"; // gif credit: https://tenor.com/view/cat-kitty-gif-25340141
-      text.innerHTML = "Sedihhh 😭😭";
-      yes.style.height = "80%";
-      yes.style.width = "80%";
-      no.style.fontSize = "4vh";
-      no.style.width = "10%";
-      count++;
-    } else if (count == 5) {
-      gif.src = "resources/speed.gif"; // gif credit: https://tenor.com/view/cat-gif-10173437195524493032
-      text.innerHTML = ":)";
-      yes.style.height = "90%";
-      yes.style.width = "96%";
+    });
+
+    yes.addEventListener("click", () => {
+      vid.style.display = "block";
+
+      // fade-in last picture
+      gif.style.opacity = 0;
+      gif.src = "resources/lastpic.JPEG";
+      gif.onload = () => {
+        gif.style.opacity = 1;
+      };
+
+      text.innerHTML =
+        "Happy Valentine bae, thanks for being in my life, love youu <3. PS: I made this at 5am karna kebangun, so I hope you like it hehe.";
+      text.style.fontSize = "1.2rem";
+
+      // geser text ke bawah
+      textBox.style.alignItems = "flex-end";
+      textBox.style.paddingTop = "150px";
+
       no.style.display = "none";
-    }
-  });
+      yes.style.display = "none";
 
-  yes.addEventListener("click", () => {
-    vid.style.display = "block";
-    gif.src = "resources/lastpic.JPEG";
-
-    text.innerHTML =
-      "Happy Valentine bae, thanks for being in my life, love youu <3. PS: I made this at 5am karna kebangun, so I hope you like it hehe.";
-
-    text.style.fontSize = "1.2rem";
-
-    // 🔥 bikin teks turun
-    const textBox = document.querySelector(".text-one");
-    textBox.style.alignItems = "flex-end";
-    textBox.style.paddingTop = "150px";
-
-    no.style.display = "none";
-    yes.style.display = "none";
-
-    setTimeout(() => {
-      vid.style.display = "none";
-    }, 9000);
+      setTimeout(() => {
+        vid.style.display = "none";
+      }, 9000);
+    });
   });
 });
